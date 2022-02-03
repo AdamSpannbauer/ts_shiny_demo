e850846f0becbd8282ce2d11b04bdf7da1e7a9ef
Adam Spannbauer <SpannbauerAdam@gmail.com>
2022-02-03 09:26:32 -0500

    add date range picker

--- ui.R
+++ ui.R
@@ -11,6 +11,14 @@ ui <- fluidPage(
     label = "Select SKU:",
     choices = unique(sales_df$pcode)
   ),
+  dateRangeInput(
+    inputId = 'selected_date_range',
+    label = "Select date range:",
+    start = min(sales_df$date),
+    end = max(sales_df$date),
+    min = min(sales_df$date),
+    max = max(sales_df$date),
+  ),
   plotOutput("ts_plot"),
   verbatimTextOutput("debug")
 )
